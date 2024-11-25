e8ce60c42549a172f4427e57645a538c22572cc9
isp41 <isp41@LAB17-11>
2024-11-25 12:02:38 +0300

    sdfs

--- gmnae[/процедураДобавитьСклад.sql
+++
@@ -1,8 +0,0 @@
---Процедура для добавления нового складского отделения
-CREATE PROCEDURE ДобавитьСклад
-    @Местоположение VARCHAR(100)
-AS
-BEGIN
-    INSERT INTO Склад (Location)
-    VALUES (@Местоположение);
-END;
